2510835c642a67528ccc15f78eca542ccde37d6e
KikeGonRam <kikeramirez160418@gmail.com>
2026-04-17 10:03:34 -0600

    Correcion de iconos y fonts

--- app/idiomas.ts
+++ app/idiomas.ts
@@ -2,11 +2,13 @@ import { KoHo } from "next/font/google";
 
 // Iconos de banderas para los idiomas soportados
 export const flags: Record<string, string> = {
-  en: '/flags/gb.svg', // Inglés (Reino Unido)
-  fr: '/flags/fr.svg', // Francés
-  de: '/flags/de.svg', // Alemán
-  it: '/flags/it.svg', // Italiano
-  pt: '/flags/pt.svg', // Portugués
+  en: '/web/flags/gb.svg', // Inglés (Reino Unido)
+  fr: '/web/flags/fr.svg', // Francés
+  de: '/web/flags/de.svg', // Alemán
+  it: '/web/flags/it.svg', // Italiano
+  pt: '/web/flags/pt.svg', // Portugués
+  es: '/web/flags/mx.svg', // Español (México)
+  mx: '/web/flags/mx.svg', // México
 };
 
 export const idiomas = [
